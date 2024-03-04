af868ac0a12222cdc67589d05e0f93ab75560327
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-03-04 14:10:35 +0100

    dashboard Admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,13 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Home from './components/CoteAdmin/pages/Home.jsx';
+import Culture from './components/CoteAdmin/pages/Culture.jsx';
+import Betail from './components/CoteAdmin/pages/Betail.jsx';
+import Agriculteurs from './components/CoteAdmin/pages/Agriculteurs.jsx';
+import Stock from './components/CoteAdmin/pages/Stock.jsx';
+import Facture from './components/CoteAdmin/pages/Facture.jsx';
+import Profile from './components/CoteAdmin/pages/Profile.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,6 +22,14 @@ root.render(
     <Routes>
           <Route path="/" element={<App />} />
           <Route path="/accueil" element={<Accueil />} />
+                {/* Admin Routes */}
+          <Route path="/home" element={<Home/>} />
+          <Route path="/culture" element={<Culture/>} />
+          <Route path="/betail" element={<Betail />} />
+          <Route path="/agriculteurs" element={<Agriculteurs />} />
+          <Route path="/stock" element={<Stock />} />
+          <Route path="/facture" element={<Facture />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
     </Router>
   </React.StrictMode>
