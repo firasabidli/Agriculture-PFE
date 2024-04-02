1b0ac72806baa449aae49f0fad9a5ec010571cc0
IssaniRania <raniaissani83@gmail.com>
2024-04-03 00:01:08 +0100

    envoyer remarque admin

--- backend/Router/Agriculture/RemarqueAgriculture.js
+++ backend/Router/Agriculture/RemarqueAgriculture.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const RemarqueController = require('../../Controller/Agriculture/RemarqueAgriculture');
 router.post('/',RemarqueController.create );
+router.get('/',RemarqueController.getByDate);
+router.delete('/:id',RemarqueController.delete);
 module.exports = router;
