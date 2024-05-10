8b6d5afb06d8fda98644995c9c30cc938674e480
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-05-10 15:47:00 +0100

    ajouter une relation plusieur a plusier entre CategorieAgriculture et Equipements , Methode de stockage, Engrais agricole

--- backend/Model/Agriculture/CategorieModel.js
+++ backend/Model/Agriculture/CategorieModel.js
@@ -4,7 +4,10 @@ const categorieSchema = new mongoose.Schema({
     nom_categorie: { type: String, required: true },
     description:{ type: String, required: true },
     Agricultures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Agriculture' }],
-    FicheAgriculture: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FicheAgriculture' }]
+    FicheAgriculture: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FicheAgriculture' }],
+    Equipements:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Materiel' }],
+    MethodeStockage:[{ type: mongoose.Schema.Types.ObjectId, ref: 'MethodeStock' }],
+    Engrais:[{ type: mongoose.Schema.Types.ObjectId, ref: 'MedicamentCulture' }]
 });
 
 module.exports = mongoose.model('CategorieAgriculture', categorieSchema);
--- backend/Model/Agriculture/MethodeStock.js
+++ backend/Model/Agriculture/MethodeStock.js
@@ -3,7 +3,8 @@ const StockSchema = new mongoose.Schema({
   title: { type: String, required: true },
   image_MethodStock: { type: String, required: true },
   description: { type: String, required: true },
-  Agricultures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Agriculture' }]
+  Agricultures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Agriculture' }],
+  CategorieAgriculture:[{ type: mongoose.Schema.Types.ObjectId, ref: 'CategorieAgriculture' }]
 });
 
 const MethodeStockModel = mongoose.model('MethodeStock', StockSchema);
