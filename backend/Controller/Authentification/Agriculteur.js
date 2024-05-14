e2ecd033925107dc6af6ef9cea356093329e8895
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-05-14 15:05:08 +0100

    dashboard Admin avec Statistique

--- backend/Controller/Authentification/Agriculteur.js
+++ backend/Controller/Authentification/Agriculteur.js
@@ -70,3 +70,57 @@ exports.getAgriculteur = async (req, res) => {
     }
   };
   
+ 
+
+// Contrôleur pour obtenir le nombre d'agriculteurs pour chaque gouvernorat
+exports.getNombreAgriculteursParGouvernorat = async (req, res) => {
+  try {
+    // Liste des gouvernorats à tester
+    const gouvernorats = [
+      { nom: 'Ariana', latitude: 36.9833, longitude: 10.1167 },
+      { nom: 'Béja', latitude: 36.7256, longitude: 9.1817 },  
+      { nom: 'Ben Arous', latitude: 36.7531, longitude: 10.2189 },  
+      { nom: 'Bizerte', latitude: 37.0833, longitude: 9.5833 },
+      { nom: 'Gabès', latitude: 33.8333, longitude: 9.75 },
+      { nom: 'Gafsa', latitude: 34.5, longitude: 9 },  
+      { nom: 'Jendouba', latitude: 36.6667, longitude: 8.75 }, 
+      { nom: 'Kairouan', latitude: 35.5833, longitude: 9.8333 }, 
+      { nom: 'Kasserine', latitude: 35.25, longitude: 8.7833 },
+      { nom: 'Kébili', latitude: 33.5, longitude: 8.8333 }, 
+      { nom: 'Le Kef', latitude: 36.0833, longitude: 8.75 }, 
+      { nom: 'Mahdia', latitude: 35.3333, longitude: 10.5833 }, 
+      { nom: 'La Manouba', latitude: 36.8333, longitude: 9.8333 }, 
+      { nom: 'Médenine', latitude: 33.3333, longitude: 11 }, 
+      { nom: 'Monastir', latitude: 35.6167, longitude: 10.75 },
+      { nom: 'Nabeul', latitude: 36.6667, longitude: 10.6667 }, 
+      { nom: 'Sfax', latitude: 34.75, longitude: 10.4167 }, 
+      { nom: 'Sidi Bouzid', latitude: 35.0382, longitude: 9.4849 }, 
+      { nom: 'Siliana', latitude: 36, longitude: 9.3333 }, 
+      { nom: 'Sousse', latitude: 35.9167, longitude: 10.4167 }, 
+      { nom: 'Tataouine', latitude: 32, longitude: 10 }, 
+      { nom: 'Tozeur', latitude: 34, longitude: 8.0833 }, 
+      { nom: 'Tunis', latitude: 36.7667, longitude: 10.1333 }, 
+      { nom: 'Zaghouan', latitude: 36.3333, longitude: 10 }, 
+    ];
+
+    // Initialiser un objet pour stocker le nombre d'agriculteurs pour chaque gouvernorat
+    const nombreAgriculteursParGouvernorat = {};
+
+    // Boucle à travers chaque gouvernorat
+    for (const gouvernorat of gouvernorats) {
+      // Compter le nombre d'agriculteurs pour le gouvernorat en cours
+      const count = await Utilisateur.Agriculteur.countDocuments({
+        'gouvernorat.nom': gouvernorat.nom,
+      });
+
+      // Ajouter le nombre d'agriculteurs au gouvernorat correspondant dans l'objet
+      nombreAgriculteursParGouvernorat[gouvernorat.nom] = count;
+    }
+
+    // Répondre avec l'objet contenant le nombre d'agriculteurs pour chaque gouvernorat
+    res.json(nombreAgriculteursParGouvernorat);
+  } catch (error) {
+    console.error('Erreur lors de la récupération du nombre d\'agriculteurs par gouvernorat :', error);
+    res.status(500).json({ message: 'Une erreur est survenue lors du traitement de la requête.' });
+  }
+};
--- frontend/src/components/CoteAdmin/pages/Main.jsx
+++ frontend/src/components/CoteAdmin/pages/Main.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
 
+import {React, useEffect, useState} from 'react';
+import axios from 'axios';
+import { GiCow, GiFarmer } from 'react-icons/gi';
+import { GiWheat } from "react-icons/gi";
+import  Charts  from './Chart';
 const Main = () => {
+    const [betail, setBetail] = useState([]);
+    const [agriculteurs, setAgriculteurs] = useState([]);
+    const [agricultures, setAgricultures] = useState([]);
+    const fetchBetail = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/Betail');
+            if (Array.isArray(response.data.data)) {
+                setBetail(response.data.data);
+               
+            } else {
+                console.error('La réponse de l\'API ne contient pas de tableau de données:', response.data.data);
+            }
+        } catch (error) {
+            console.error('Erreur lors du chargement des données:', error);
+        }
+    };
+
+    const fetchAgriculteurs = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/Agriculteur/user');
+            if (Array.isArray(response.data)) {
+                setAgriculteurs(response.data);
+              
+            } else {
+                console.error('La réponse de l\'API ne contient pas de tableau de données:', response.data);
+            }
+        } catch (error) {
+            console.error('Erreur lors du chargement des données:', error);
+        }
+    };
+
+    const fetchAgricultures = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/Agriculture');
+            if (Array.isArray(response.data.data)) {
+                setAgricultures(response.data.data);
+                
+            } else {
+                console.error('La réponse de l\'API ne contient pas de tableau de données:', response.data.data);
+            }
+        } catch (error) {
+            console.error('Erreur lors du chargement des données:', error);
+        }
+    };
+    
+    useEffect(() => {
+        fetchBetail();
+        fetchAgriculteurs();
+        fetchAgricultures();
+    }, []);
     return (
-        <main className='main-container'>
-        <div className='main-title'>
-            <h3>DASHBOARD</h3>
-        </div>
+        <main className='main-container p-3 m-5 resCont'>
+        <div class="row row-cols-1 mb-3 ">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-3 "  >
+                <div class="card" style={{borderRadius:'5%'}}>
+                <div class="card-body"style={{color:'white',backgroundColor:'#38c609',borderRadius:'5%'}}>
+                    <h2 class="card-title text-center text-light"> <GiFarmer style={{fontSize:'50px'}}/></h2>
+                    <h5 class="card-title text-center text-light"> Agriculteurs</h5>
+                    
+                    <h5 className='text-center'>{agriculteurs.length}</h5>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-3 "  >
+                <div class="card" style={{borderRadius:'5%'}}>
+                <div class="card-body"style={{color:'white',backgroundColor:'#38c609',borderRadius:'5%'}}>
+                    <h2 class="card-title text-center text-light"> <GiCow style={{fontSize:'50px'}}/></h2>
+                    <h5 class="card-title text-center text-light"> Bétails</h5>
+                    
+                    <h5 className='text-center'>{betail.length}</h5>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-3 "  >
+                <div class="card" style={{borderRadius:'5%'}}>
+                <div class="card-body"style={{color:'white',backgroundColor:'#38c609',borderRadius:'5%'}}>
+                    <h2 class="card-title text-center text-light"> <GiWheat  style={{fontSize:'50px'}}/></h2>
+                    <h5 class="card-title text-center text-light"> cultures</h5>
+                    
+                    <h5 className='text-center'>{agricultures.length}</h5>
+                </div>
+                </div>
+            </div>
+          
+            </div>
+            <Charts agricultures={agricultures.length} betails={betail}/>
         </main>
     );
 };
